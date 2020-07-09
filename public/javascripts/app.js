ee13a2b50714615402a3d7ffae150576126e43e6
Madhuparna Bhowmik <madhuparnab@google.com>
2020-07-09 19:29:07 +0530

    UI Improved

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,21 +4,47 @@ let searchConfig = {
   "searchAppId": "searchapplications/e96ae9836b91bbe7954835ee35a098f3"
 };
 
-// Reference to the results container to clear results on login/logout changes.
 let resultsContainer;
 
-/**
- * Load the cloud search widget & auth libraries. Runs after
- * the initial gapi bootstrap library is ready.
- */
 function onLoad() {
   gapi.load('client:auth2:cloudsearch-widget', initializeApp);
 }
 
-/**
- * Initialize the app after loading the Google API client &
- * Cloud Search widget.
- */
+function ResultsContainerAdapter() {
+  this.selectedSource = null;
+}
+
+ResultsContainerAdapter.prototype.decorateFacetResultElement = function(element, result) {
+    for(var i=1; i<element.childNodes.length; ++i) {
+        element.childNodes[i].childNodes[1].style.color="#000000";
+        element.childNodes[i].childNodes[2].textContent = result.buckets[i-1].count;//result.objectType;//setAttribute("style", "color:green;");
+    }
+    var text = element.childNodes[0].childNodes[0].textContent;
+    element.childNodes[0].childNodes[0].setAttribute("style", "color:#440066;");
+    switch(text) {
+        case "authorname" : element.childNodes[0].childNodes[0].textContent = "Author Names";
+        break;
+        case "disease" : element.childNodes[0].childNodes[0].textContent = "Diseases";
+        break;
+        case "org" : element.childNodes[0].childNodes[0].textContent = "Organizations";
+        break;
+        case "symptom" : element.childNodes[0].childNodes[0].textContent = "Symptoms";
+        break;
+        case "virus" : element.childNodes[0].childNodes[0].textContent = "Virus";
+        break;
+        case "pathogen" : element.childNodes[0].childNodes[0].textContent = "Pathogens";
+        default:
+        break;
+    }
+}
+
+ResultsContainerAdapter.prototype.decorateSearchResultElement = function(element, result) {
+element.setAttribute("style", "border-radius: 25px; margin: 25px 0px 25px 0px;");
+element.childNodes[1].childNodes[0].style.color  = "#440066";
+console.log(element);
+console.log(result);
+}
+
 async function initializeApp() {
   await gapi.auth2.init({
       'clientId': searchConfig.clientId,
@@ -38,17 +64,16 @@ async function initializeApp() {
   }
 
   auth.isSignedIn.listen(onSignInChanged);
-  onSignInChanged(auth.isSignedIn.get()); // Trigger with current status
+  onSignInChanged(auth.isSignedIn.get());
 
-  // Connect sign-in/sign-out buttons
-  document.getElementById("sign-in").onclick = (e) =>  auth.signIn();
+=  document.getElementById("sign-in").onclick = (e) =>  auth.signIn();
   document.getElementById("sign-out").onclick = (e) => auth.signOut();
-  // [END cloud_search_tutorial_init_auth]
+   var resultsContainerAdapter = new ResultsContainerAdapter();
 
-  // [START cloud_search_tutorial_init_widget]
   gapi.config.update('cloudsearch.config/apiVersion', 'v1');
   resultsContainer = new gapi.cloudsearch.widget.resultscontainer.Builder()
     .setSearchApplicationId(searchConfig.searchAppId)
+    .setAdapter(resultsContainerAdapter)
     .setSearchResultsContainerElement(document.getElementById('search_results'))
     .setFacetResultsContainerElement(document.getElementById('facet_results'))
     .build();
